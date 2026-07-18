3ce441b6b9ab5967c148ab2ea64f817733f4e8d7
Assakorn <69070326@kmitl.ac.th>
2026-07-18 12:51:03 +0700

    69070326

--- Learning Logs/3036/main.py
+++ Learning Logs/3036/main.py
@@ -1,13 +1,28 @@
 """CASTLE"""
 
-NUMBER = int(input())
-ROW = 0
+import math
 
-while True:
-    MAX_Col = ROW * ROW
-    if MAX_Col < NUMBER:
-        ROW += 1
+room = int(input())
+
+# หาแถวของห้อง
+row = math.ceil(math.sqrt(room))
+
+answer = 0
+
+while room > 1:
+
+    # หาตำแหน่งของห้องในแถว
+    position = room - (row - 1) ** 2
+
+    # ขึ้นไปแถวบน
+    if position % 2 == 0:
+        room = (row - 1) ** 2 + position // 2
     else:
-        if 
+        room = (row - 1) ** 2 + (position + 1) // 2
 
-# print(ROW)
+    answer += 1
+
+    # หาแถวใหม่หลังจากขึ้น
+    row = math.ceil(math.sqrt(room))
+
+print(answer)
